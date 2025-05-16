408e1223e6b2c164d3e892a7c48738a96ee716de
Echoecho01 <jericho011530@gmail.com>
2025-05-16 21:20:11 +0800

    commit

--- test_voter_checker.py
+++ test_voter_checker.py
@@ -5,16 +5,16 @@ from voter_checker import is_eligible_to_vote
 class TestVoterEligibility(unittest.TestCase):
 
     def test_underage_voter(self):
-        self.assertFalse(self.underage_voter(17, True))
+        self.assertFalse(is_eligible_to_vote(17, True))
 
     def test_non_citizen_voter(self):
-        self.assertFalse(self.is_eligible_to_vote(25, False))
+        self.assertFalse(is_eligible_to_vote(25, False))
 
     def test_underage_non_citizen(self):
-        self.assertFalse(self.is_eligible_to_vote(16, False))
+        self.assertFalse(is_eligible_to_vote(16, False))
 
     def test_exactly_18_and_non_citizen(self):
-        self.assertFalse(self.is_eligible_to_vote(18, False))
+        self.assertFalse(is_eligible_to_vote(18, False))
 
     def test_negative_age(self):
         with self.assertRaises(ValueError):
