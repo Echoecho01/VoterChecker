3c03e4585ee5b8aadbe2343c8c8dd75d42b059f2
Echoecho01 <jericho011530@gmail.com>
2025-05-16 20:07:27 +0800

    commit

--- test_voter_checker.py
+++ test_voter_checker.py
@@ -2,10 +2,16 @@ import unittest
 from voter_checker import is_eligible_to_vote
 
 class TestVoterEligibility(unittest.TestCase):
-
+    
 def test_underage_voter(self):
     self.assertFalse(self.underage_voter(17,True))
-    ("underage")
+
 def test_non_citizen_voter(self):
     self.assertFalse(self.is_eligible_to_vote(25,False))
+
+def test_underage_non_citizen(self):
+    self.assertFalse(self.is_eligible_to_vote(16,False))
+    
+
+
 
